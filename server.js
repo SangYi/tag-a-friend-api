2722539af87d63b66d72a24b17da0c9ddb8d1faa
Sang Yi <imsangyi@gmail.com>
2018-03-28 16:43:39 -0500

    knex command for register and practice with jwt

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex')
 
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 const register = require('./controllers/register');
 const login = require('./controllers/login');
 const image = require('./controllers/image');
@@ -15,7 +16,7 @@ const db = knex({
         host : '127.0.0.1',
         user : 'postgres',
         password : 'yellow45',
-        database : 'smart-brain'
+        database : 'test'
     }
 });
 
@@ -38,6 +39,92 @@ app.post('/login', (req, res) => {
 
 app.post('/imageurl', image.handleApiCall)
 
+app.post('/testregister', (req, res) => {
+    const {email, username, name, password} = req.body;
+    const hash = bcrypt.hashSync(password)
+    //temp
+    db.transaction(trx => {
+        trx('login').insert({
+            hash,
+            email,
+            username
+        }, 'email')
+        .then( ([loginEmail]) => {
+            console.log(loginEmail);
+            return trx('account')
+                .insert({
+                    email: loginEmail,
+                    // name,
+                    username,
+                    created_on: new Date()
+                }, '*')
+                .then( ([user]) => {
+                    console.log(user)
+                    res.json(user)
+                })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch(err => res.status(400).json({
+        message: 'unable to register',
+        err
+    }))
+})
+//---------
+app.get('/api', (req, res) => {
+    res.json({
+        message: "welcome to the API"
+    });
+});
+app.post('/api/posts', veriftyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            console.log(err)
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            });
+        }
+    });
+});
+app.post('/api/login', (req, res) => {
+    //stuff happened here and got user back
+    const user = {
+        id: 1,
+        username: 'sang',
+        email: 'sang@gmail.com'
+    }
+    jwt.sign({user}, 'secretkey', {expiresIn: '30s'}, (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+//FORMAT OF TOKEN
+//Authorization : Bearer <access_token>
+//Verifty Token
+function veriftyToken(req, res, next) {
+    //Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(bearerHeader) {
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middleware
+        next()
+    } else {
+        // Forbidden
+        res.sendStatus(403)
+    }
+}
+//---------
 app.listen(PORT, ()=> {
     console.log(`App is running on port ${PORT}`);
 });
